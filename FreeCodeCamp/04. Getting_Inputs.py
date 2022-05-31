92700e2ba87b9757d0fa1b9ffb6e57485b4b7dc7
DesigneR- <limjunyi03@gmail.com>
2022-05-31 15:43:53 +0800

    Create 04. Getting_Inputs.py

---
+++ FreeCodeCamp/04. Getting_Inputs.py
@@ -0,0 +1,6 @@
+# Getting User Inputs
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))    # Converts the input from str to int
+
+print("Your name is " + name + " with the age of " + str(age) + ".")
