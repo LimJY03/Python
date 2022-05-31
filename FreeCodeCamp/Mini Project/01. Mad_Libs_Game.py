604656cbb7ee66f53a30a738e437fdd99d9dcdaf
DesigneR- <limjunyi03@gmail.com>
2022-05-31 16:05:59 +0800

    Create 01. Mad_Libs_Game.py

---
+++ FreeCodeCamp/Mini Project/01. Mad_Libs_Game.py
@@ -0,0 +1,18 @@
+s1 = input("Enter an adjective: ")
+s2 = input("Enter a noun: ")
+s3 = input("Enter a plural noun: ")
+s4 = input("Ehter a female: ")
+s5 = input("Enter an adjective: ")
+s6 = input("Enter an article about clothing: ")
+s7 = input("Enter a noun: ")
+s8 = input("Enter a city name: ")
+s9 = input("Enter a plural noun: ")
+s10 = input("Enter an adjective: ")
+s11 = input("Enter a body part: ")
+s12 = input("Enter an alphabet: ")
+s13 = input("Enter a celebrity name: ")
+s14 = input("Enter a plural noun: ")
+
+paragraph = "There are many " + s1 + " ways to choose a/an " + s2 +  " to read. First, you could ask for recommendations from your friends and " + s3 + ". Just don't ask Aunt " + s4 + " - she only reads " + s5 + " books with " + s6 + " - ripping goddesses on the cover. If your friends and family are no help, try checking out the " + s7 + " Review in The " + s8 + " Times. If the " + s9 + " featured there are too " + s10 + " for your taste, try something a little more low-" + s11 + ", like " + s12 + ": The " + s13 + " Magazine, or " + s14 + "Magazine."
+
+print(paragraph)
