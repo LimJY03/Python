5def3ec5f4d6a415ff932016d03b59877088a272
DesigneR- <limjunyi03@gmail.com>
2022-06-04 18:53:45 +0800

    Create 14. Class_and_Objects.py

---
+++ FreeCodeCamp/14. Class_and_Objects.py
@@ -0,0 +1,15 @@
+# Classes and Objects
+
+# Importing Students Class
+
+from Student_Class import students  
+
+# Creating Objects
+
+s1 = students('Jim', 21, 'Computer Science', 3.86, False)
+s2 = students('Ben', 19, 'Photography', 4.00, False)
+s3 = students('Amy', 25, 'Software Engineer', 3.72, True)
+
+print(s1.name)      # Jim
+print(s2.age)       # 19
+print(s3.major)     # Software Engineer
