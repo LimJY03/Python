daedb65888b0131c69ac031fd366fd0199d58f94
DesigneR- <limjunyi03@gmail.com>
2022-06-04 20:34:24 +0800

    Update 14. Class_and_Objects.py

--- FreeCodeCamp/14. Class_and_Objects.py
+++ FreeCodeCamp/14. Class_and_Objects.py
@@ -2,14 +2,22 @@
 
 # Importing Students Class
 
-from Student_Class import students  
+from Custom_Class import students  
 
 # Creating Objects
 
-s1 = students('Jim', 21, 'Computer Science', 3.86, False)
-s2 = students('Ben', 19, 'Photography', 4.00, False)
-s3 = students('Amy', 25, 'Software Engineer', 3.72, True)
+s1 = students('Jim', 3, 'Computer Science', 3.86, False)
+s2 = students('Ben', 2, 'Photography', 4.00, False)
+s3 = students('Amy', 5, 'Software Engineer', 3.72, True)
 
-print(s1.name)      # Jim
-print(s2.age)       # 19
-print(s3.major)     # Software Engineer
+print(s1.name)                  # Jim
+print(s2.year)                  # 2
+print(s3.major)                 # Software Engineer
+
+print(s1.should_honor())        # False
+print(s2.should_honor())        # True
+print(s3.should_honor())        # False
+
+print(s1.years_to_graduate())   # 1 more year to go.
+print(s2.years_to_graduate())   # 2 more years to go.
+print(s3.years_to_graduate())   # Should graduate.
