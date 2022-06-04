bea0118d7a389c1a3cf1c176a896d699ff4a56fa
DesigneR- <limjunyi03@gmail.com>
2022-06-04 18:54:11 +0800

    Added Student_Class for importing

---
+++ FreeCodeCamp/Student_Class.py
@@ -0,0 +1,9 @@
+class students:
+
+    def __init__(self, name, age, major, gpa, is_married):
+
+        self.name = name
+        self.age = age
+        self.major = major
+        self.gpa = gpa
+        self.marriedstatus = is_married
