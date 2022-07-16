7df693ebcdda485dcfdd2a862c00de6ba38d40b5
Lim JY <limjunyi03@gmail.com>
2022-07-16 18:13:40 +0800

    Updated Question Formatting for Readability

--- ProblemSolving/Time_Difference.py
+++ ProblemSolving/Time_Difference.py
@@ -1,7 +1,9 @@
 '''
-When users post an update on social media,such as a URL, image, status update etc., other users in their network are able to view this new post on their news feed. Users can also see exactly when the post was published, i.e, how many hours, minutes or seconds ago.
+When users post an update on social media,such as a URL, image, status update etc., other users in their network are able to view this new post 
+on their news feed. Users can also see exactly when the post was published, i.e, how many hours, minutes or seconds ago.
 
-Since sometimes posts are published and viewed in different time zones, this can be confusing. You are given two timestamps of one such post that a user can see on his newsfeed in the following format:
+Since sometimes posts are published and viewed in different time zones, this can be confusing. You are given two timestamps of one such post 
+that a user can see on his newsfeed in the following format:
 
 [Day dd Mon yyyy hh:mm:ss +xxxx]
 
@@ -37,7 +39,8 @@ Sample Output 0
 Explanation 0
 
 > In the first query, when we compare the time in UTC for both the time stamps, we see a difference of 7 hours. which is 7 * 3600 seconds or 25200 seconds.
-> Similarly, in the second query, time difference is 5 hours and 30 minutes for time zone adjusting for that we have a difference of 1 day and 30 minutes. Or 24 * 3600 + 30 * 60 = 88200.
+> Similarly, in the second query, time difference is 5 hours and 30 minutes for time zone adjusting for that we have a difference of 1 day and 30 minutes. 
+  Or 24 * 3600 + 30 * 60 = 88200.
 
 src: "https://www.hackerrank.com/challenges/python-time-delta/problem"
 '''
