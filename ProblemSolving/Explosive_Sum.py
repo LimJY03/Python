7abed611a8c9608d47a9d7bc5e0c0e275522c939
DesigneR- <limjunyi03@gmail.com>
2022-10-26 17:35:22 +0800

    Create Explosive_Sum.py

---
+++ ProblemSolving/Explosive_Sum.py
@@ -0,0 +1,83 @@
+'''
+How many ways can you make the sum of a number?
+From wikipedia: https://en.wikipedia.org/wiki/Partition_(number_theory)
+
+In number theory and combinatorics, a partition of a positive integer n, also called an integer partition, 
+is a way of writing n as a sum of positive integers. 
+
+Two sums that differ only in the order of their summands are considered the same partition. If order matters, 
+the sum becomes a composition. For example, 4 can be partitioned in five distinct ways:
+
+> 4
+> 3 + 1
+> 2 + 2
+> 2 + 1 + 1
+> 1 + 1 + 1 + 1
+
+Examples
+
+> Basic
+    > exp_sum(1) # 1
+    > exp_sum(2) # 2  -> 1+1 , 2
+    > exp_sum(3) # 3 -> 1+1+1, 1+2, 3
+    > exp_sum(4) # 5 -> 1+1+1+1, 1+1+2, 1+3, 2+2, 4
+    > exp_sum(5) # 7 -> 1+1+1+1+1, 1+1+1+2, 1+1+3, 1+2+2, 1+4, 5, 2+3
+    > ...
+    > exp_sum(10) # 42
+> Explosive
+    > exp_sum(50) # 204226
+    > exp_sum(80) # 15796476
+    > exp_sum(100) # 190569292
+
+src: "https://www.codewars.com/kata/52ec24228a515e620b0005ef/train/python"
+'''
+
+def pattern(memory: list) -> int:
+
+    temp, current, pair, diff = 0, len(memory) - 1, 1, 3
+    isPair, isAdd = True, True
+
+    while current >= 0:
+
+        if isPair:
+
+            if isAdd: temp += memory[current]
+            else: temp -= memory[current]
+
+            current = current - pair
+            
+            if current >= 0:
+
+                if isAdd: temp += memory[current]
+                else: temp -= memory[current]
+                
+            else: break
+
+            pair += 1
+
+            isPair = False
+
+        else:
+
+            current = current - diff
+
+            if current < 0: break
+
+            diff += 2
+
+            isPair, isAdd = True, not isAdd
+
+    memory.append(temp)
+
+    return memory
+
+def exp_sum(n: int) -> int:
+
+    memory = [1, 1, 2, 3]
+
+    while len(memory) - 1 < n: 
+        memory = pattern(memory)
+
+    return memory[n]
+
+print(exp_sum(100))
